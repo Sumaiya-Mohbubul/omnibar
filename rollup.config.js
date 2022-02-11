77ac77f97fcf1024cf355c218510879ee0519e7a
Sumaiya Mohbubul <sumaiya.mohbubul@kcl.ac.uk>
2022-02-11 13:18:03 +0000

    Update React to v17

--- rollup.config.js
+++ rollup.config.js
@@ -2,16 +2,16 @@ import path from 'path';
 import typescript from 'rollup-plugin-typescript';
 
 export default {
-  input: path.resolve(__dirname, 'src', 'index.tsx'),
-  output: {
-    exports: 'named',
-    file: path.resolve(__dirname, 'dist', 'index.js'),
-    format: 'cjs',
-  },
-  plugins: [
-    typescript({
-      typescript: require('typescript'),
-    }),
-  ],
-  external: ['react'],
+input: path.resolve(**dirname, 'src', 'index.tsx'),
+output: {
+exports: 'named',
+file: path.resolve(**dirname, 'dist', 'index.js'),
+format: 'cjs',
+},
+plugins: [
+typescript({
+typescript: require('typescript'),
+}),
+],
+external: ['react'],
 };
--- src/modifiers/anchor/AnchorRenderer.tsx
+++ src/modifiers/anchor/AnchorRenderer.tsx
@@ -1,54 +1,54 @@
-import * as React from 'react';
+import \* as React from 'react';
 import { AnchorItem } from './';
 import { COLORS, DEFAULT_HEIGHT } from '../../constants';
 
 interface Props<T> {
-  // the item
-  item: AnchorItem & T;
-  isSelected?: boolean;
-  isHighlighted?: boolean;
+// the item
+item: AnchorItem & T;
+isSelected?: boolean;
+isHighlighted?: boolean;
 }
 
 const ITEM_STYLE: React.CSSProperties = {
-  borderBottomWidth: 1,
-  borderColor: COLORS.DARKGRAY,
-  borderLeftWidth: 1,
-  borderRightWidth: 1,
-  borderStyle: 'solid',
-  borderTopWidth: 0,
-  boxSizing: 'border-box',
-  color: COLORS.BLACK,
-  display: 'block',
-  fontSize: 24,
-  height: DEFAULT_HEIGHT,
-  lineHeight: `${DEFAULT_HEIGHT}px`,
-  paddingLeft: 15,
-  paddingRight: 15,
-  textDecoration: 'none',
+borderBottomWidth: 1,
+borderColor: COLORS.DARKGRAY,
+borderLeftWidth: 1,
+borderRightWidth: 1,
+borderStyle: 'solid',
+borderTopWidth: 0,
+boxSizing: 'border-box',
+color: COLORS.BLACK,
+display: 'block',
+fontSize: 24,
+height: DEFAULT_HEIGHT,
+lineHeight: `${DEFAULT_HEIGHT}px`,
+paddingLeft: 15,
+paddingRight: 15,
+textDecoration: 'none',
 };
 
 const ITEM_HOVER_STYLE: React.CSSProperties = {
-  backgroundColor: COLORS.GRAY,
+backgroundColor: COLORS.GRAY,
 };
 
 export default function AnchorRenderer<T>(
-  props: Props<T> & React.HTMLAttributes<HTMLAnchorElement>
+props: Props<T> & React.HTMLAttributes<HTMLAnchorElement>
 ) {
-  const { item, isSelected, isHighlighted, style, ...rest } = props;
+const { item, isSelected, isHighlighted, style, ...rest } = props;
 
-  const mergedStyle = { ...ITEM_STYLE, ...style };
+const mergedStyle = { ...ITEM_STYLE, ...style };
 
-  if (isSelected) {
-    mergedStyle.backgroundColor = COLORS.GRAY;
-  }
+if (isSelected) {
+mergedStyle.backgroundColor = COLORS.GRAY;
+}
 
-  if (isHighlighted) {
-    mergedStyle.backgroundColor = COLORS.DARKGRAY;
-  }
+if (isHighlighted) {
+mergedStyle.backgroundColor = COLORS.DARKGRAY;
+}
 
-  return (
-    <a href={item.url} style={mergedStyle} {...rest}>
-      {item.title}
-    </a>
-  );
+return (
+<a href={item.url} style={mergedStyle} {...rest}>
+{item.title}
+</a>
+);
 }
